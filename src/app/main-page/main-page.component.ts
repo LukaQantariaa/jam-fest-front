b2a5bc96b5c9558199d2d27e48adf54aec5ea03d
Gundavri <m.mirianashvili99@gmail.com>
2019-11-17 03:57:54 +0400

    added favorite pics

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -8,36 +8,16 @@ import { Component, OnInit } from '@angular/core';
 export class MainPageComponent implements OnInit {
 
   favoriteItemList = [ 
-    {
-      img: '../../assets/pictures/most-popular/item1.jpg'
-    },
-    {
-      img: '../../assets/pictures/most-popular/item1.jpg'
-    },
-    {
-      img: '../../assets/pictures/most-popular/item1.jpg'
-    },
-    {
-      img: '../../assets/pictures/most-popular/item1.jpg'
-    },
-    {
-      img: '../../assets/pictures/most-popular/item1.jpg'
-    },
-    {
-      img: '../../assets/pictures/most-popular/item1.jpg'
-    },
-    {
-      img: '../../assets/pictures/most-popular/item1.jpg'
-    },
-    {
-      img: '../../assets/pictures/most-popular/item1.jpg'
-    },
-    {
-      img: '../../assets/pictures/most-popular/item1.jpg'
-    },
-    {
-      img: '../../assets/pictures/most-popular/item1.jpg'
-    }
+    '../../assets/pictures/most-popular/item0.jpg',
+    '../../assets/pictures/most-popular/item1.jpg',
+    '../../assets/pictures/most-popular/item2.jpg',
+    '../../assets/pictures/most-popular/item3.jpg',
+    '../../assets/pictures/most-popular/item4.jpg',
+    '../../assets/pictures/most-popular/item5.jpg',
+    '../../assets/pictures/most-popular/item6.jpg',
+    '../../assets/pictures/most-popular/item7.jpg',
+    '../../assets/pictures/most-popular/item8.jpg',
+    '../../assets/pictures/most-popular/item9.jpg'
   ];
 
   constructor() { }
